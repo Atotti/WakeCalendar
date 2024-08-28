02ae640b6b09dfb349af705c722e4f9247910542
Ayuto Tsutsumi <56724676+Atotti@users.noreply.github.com>
2024-08-28 16:07:31 +0900

    fix

--- alarm.py
+++ alarm.py
@@ -13,7 +13,7 @@ def play_random_alarm(music_directory):
         music_path = os.path.join(music_directory, random_music_file)
         
         # Play the random mp3 file
-        command = ["mpg123", "-a", "hw:0,0", os.path.join(music_directory, music_path)]
+        command = ["mpg123", "-a", "hw:2,0", os.path.join(music_directory, music_path)]
         subprocess.run(command)
     else:
         print("MP3 files not found in the music directory.")
